c4f45267c8ddb85322d1439936210306bee0b15d
anil0403 <anilshrestha0403.as@gmail.com>
2023-09-23 21:33:01 +0545

    error fixed

--- app/[productId]/product.tsx
+++ app/[productId]/product.tsx
@@ -1,53 +1,9 @@
-<p className="mb-2 text-gray-600">
-<strong>Processor:</strong> {product?.size?.name}
-{product?.size?.value}
-{product?.core && (
-  <span>
-    | {product?.core?.core} {product?.core?.thread}
-  </span>
-)}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>RAM:</strong> {product?.ram?.name} {product?.ram?.value}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Graphics:</strong> {product?.color?.name}{" "}
-{product?.color?.value}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Storage:</strong> {product?.storage}{" "}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>WIFI | Bluetooth:</strong> {product?.wlanBluetooth}{" "}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Camera:</strong> {product?.camera}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Display:</strong> {product?.display}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Fingerprint:</strong> {product?.fingerPrint}{" "}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Keyboard:</strong> {product?.keyboard}{" "}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Ethernet:</strong> {product?.ethernet}{" "}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Battery:</strong> {product?.battery}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Power Adapter:</strong> {product?.powerAdapter}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Memory Slot:</strong> {product?.memorySlot}
-{product?.ram?.value}
-</p>{" "}
-<p className="mb-2 text-gray-600">
-<strong>Storage Slot:</strong> {product?.StorageSlot}
-</p>
-<p className="mb-2 text-gray-600">
-<strong>Free Items:</strong> {product?.freeItems}
-</p>
+import React from 'react'
+
+const product = () => {
+  return (
+    <div>product</div>
+  )
+}
+
+export default product
