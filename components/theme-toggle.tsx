09218d570b279a940830e0442922154a3ead97ba
anil0403 <anilshrestha0403.as@gmail.com>
2023-09-23 23:28:32 +0545

    theme updated

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -14,7 +14,7 @@ export function ThemeToggle() {
       variant="ghost"
       size="icon"
 
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
       <Moon className="hidden h-5 w-5 dark:block" />
